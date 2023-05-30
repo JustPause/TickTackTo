e8e82be7da3b915c4a1622b697c9a51492b20d6e
JustPause <Justinaslolt@gmail.com>
2023-05-31 02:24:13 +0300

    didint chnk it but hes

--- C++/WinnerFinder.cpp
+++ C++/WinnerFinder.cpp
@@ -4,6 +4,8 @@
 #include "UserInput.h"
 
 bool Tie();
+bool Horizontal();
+bool Vertical();
 
 bool CheckIfTheSpaceIsUsed()
 {
@@ -23,8 +25,23 @@ bool CheckIfWeHaveAWinner()
     {
         ShowCanvas();
         std::cout << "The game has finish, and it's a tie" << std::endl;
-        return Tie;
+        return Tie();
     }
+
+    else if (Horizontal())
+    {
+        ShowCanvas();
+        std::cout << "The game has finish, and winner is Player " << std::endl;
+        return Horizontal();
+    }
+
+    else if (Vertical())
+    {
+        ShowCanvas();
+        std::cout << "The game has finish, and winner is Player " << std::endl;
+        return Vertical();
+    }
+
     return false;
 }
 
@@ -45,6 +62,41 @@ bool Tie()
     {
         return true;
         // Stop The game
+    }
+    return false;
+}
+
+bool Horizontal()
+{
+    int y = 0;
+    for (int i = 0; i < CanvasX; i++)
+    {
+        if ((Canvas[i][y] == Canvas[i][y + 1] && Canvas[i][y + 1] == Canvas[i][y + 2]) && Canvas[i][y + 1] != '+')
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Vertical()
+{
+    int x = 0;
+    for (int i = 0; i < CanvasX; i++)
+    {
+        if ((Canvas[x][i] == Canvas[x + 1][i] && Canvas[x + 1][i] == Canvas[x + 2][i]) && Canvas[x + 1][i] != '+')
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Diagonal()
+{
+    if (Canvas[0][0] == Canvas[1][1] && Canvas[1][1] == Canvas[2][2] && Canvas[2][0] == Canvas[1][1] && Canvas[1][1] == Canvas[0][2])
+    {
+        return true;
     }
     return false;
 }
